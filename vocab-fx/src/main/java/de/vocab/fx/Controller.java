40d56653196b67cff1cf488ec9d2c8de50c9fe5e
Philipp <philipp.ossler@gmail.com>
2015-06-26 09:07:39 +0200

    add word to table

--- vocab-fx/src/main/java/de/vocab/fx/Controller.java
+++ vocab-fx/src/main/java/de/vocab/fx/Controller.java
@@ -15,10 +15,19 @@ public class Controller {
 
 	@FXML
 	private TableView<Word> wordTable;
+	
+	public void init(){
+		System.out.println("init");
+		
+	}
 
 	@FXML
 	public void addWord(ActionEvent event) {
 		System.out.println("add word");
-		//wordTable.getItems().add(new Word());
+		Word newWord = new Word(wordField.getText(), translationField.getText());
+		wordTable.getItems().add(newWord);
+		
+		wordField.clear();
+		translationField.clear();
 	}
 }
--- vocab-fx/src/main/java/de/vocab/fx/Word.java
+++ vocab-fx/src/main/java/de/vocab/fx/Word.java
@@ -1,9 +1,31 @@
 package de.vocab.fx;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Word {
 
-	private StringProperty word;
-	private StringProperty translation;
+	private final StringProperty word;
+	private final StringProperty translation;
+	
+	public Word(String word, String translation) {
+		this.word = new SimpleStringProperty(word);
+		this.translation = new SimpleStringProperty(translation);
+	}
+	
+	public String getWord(){
+		return word.getValue();
+	}
+	
+	public void setWord(String word){
+		this.word.setValue(word);
+	}
+	
+	public String getTranslation(){
+		return translation.getValue();
+	}
+	
+	public void setTranslation(String translation){
+		this.translation.setValue(translation);
+	}
 }
